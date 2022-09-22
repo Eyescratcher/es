a56a2ecdfdefb38fe6c0756878274f36a88b86d3
EyesCratcher <spirosc90@gmail.com>
2022-09-22 22:45:04 +0300

    Styling Final

--- src/components/category/category.component.ts
+++ src/components/category/category.component.ts
@@ -11,34 +11,10 @@ export class CategoryComponent implements OnInit {
 
   id!:number;
   categories:any;
-  arr: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-  totalCards: number = this.arr.length;
-  currentPage: number = 1;
-  pagePosition: string = "0%";
-  cardsPerPage!: number;
-  totalPages!: number;
-  overflowWidth!: string;
-  cardWidth!: string;
-  containerWidth!: number;
-  @ViewChild("container", { static: true, read: ElementRef })
-  container!: ElementRef;
-  @HostListener("window:resize") windowResize() {
-    let newCardsPerPage = this.getCardsPerPage();
-    if (newCardsPerPage != this.cardsPerPage) {
-      this.cardsPerPage = newCardsPerPage;
-      this.initializeSlider();
-      if (this.currentPage > this.totalPages) {
-        this.currentPage = this.totalPages;
-        this.populatePagePosition();
-      }
-    }
-  }
-
+  
   constructor(private categoryService:CategoryService,private shared:SharedService) { }
 
   ngOnInit() {
-    this.cardsPerPage = this.getCardsPerPage();
-    this.initializeSlider();
     this.getAllCategories();
   }
 
@@ -62,27 +38,4 @@ export class CategoryComponent implements OnInit {
   //     complete: () => console.log('complete')
   //   });
   // }
-
-  initializeSlider() {
-    this.totalPages = Math.ceil(this.totalCards / this.cardsPerPage);
-    this.overflowWidth = `calc(${this.totalPages * 100}% + ${this.totalPages *
-      10}px)`;
-    this.cardWidth = `calc((${100 / this.totalPages}% - ${this.cardsPerPage *
-      10}px) / ${this.cardsPerPage})`;
-  }
-
-  getCardsPerPage() {
-    return Math.floor(this.container.nativeElement.offsetWidth / 300);
-  }
-
-  changePage(incrementor: number) {
-    this.currentPage += incrementor;
-    this.populatePagePosition();
-  }
-
-  populatePagePosition() {
-    this.pagePosition = `calc(${-100 * (this.currentPage - 1)}% - ${10 *
-      (this.currentPage - 1)}px)`;
-  }
-
 }
